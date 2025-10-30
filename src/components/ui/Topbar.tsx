9932682f79650674cdf565e6ed66a0155aac3c82
Chris <moreirachristopher100@gmail.com>
2025-10-29 21:29:00 -0300

    prod fixes

--- src/components/ui/Topbar.tsx
+++ src/components/ui/Topbar.tsx
@@ -1,7 +1,7 @@
 import { Button } from "@components/button";
 import { Link } from "react-router-dom";
 
-export default function TopBar() {
+const TopBar:React.FC = () => {
   return (
     <header className="fixed top-0 left-0 right-0 z-50 bg-pink-200/80 backdrop-blur border-b border-pink-300">
       <div className="max-w-7xl mx-auto w-full flex items-center px-4 sm:px-6 lg:px-8 py-3">
@@ -40,3 +40,4 @@ export default function TopBar() {
     </header>
   );
 }
+export default TopBar
